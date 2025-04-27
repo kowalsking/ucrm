77625e4ca151cf06e73fc0f324ad79c12afff8e1
Hence <kovvalsking@gmail.com>
2025-04-27 10:49:38 +0300

    Laba 3

--- app/Http/Controllers/Api/V1/TasksController.php
+++ app/Http/Controllers/Api/V1/TasksController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTasksRequest;
 use App\Http\Requests\UpdateTasksRequest;
+use App\Http\Resources\TasksResource;
 use App\Models\Tasks;
+use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
@@ -14,54 +16,60 @@ class TasksController extends Controller
      */
     public function index()
     {
-        return Tasks::all();
+        return TasksResource::collection(Tasks::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTasksRequest $request)
     {
-        //
+        $tasks = Tasks::create($request->validated());
+
+        return TasksResource::make($tasks);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Tasks $tasks)
+    public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Tasks $tasks)
-    {
-        //
+        $task = Tasks::find($id);
+        return TasksResource::make($task);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTasksRequest $request, Tasks $tasks)
+    public function update(UpdateTasksRequest $request, $id)
     {
-        //
+        $tasks = Tasks::find($id);
+        $tasks->update($request->validated());
+
+        return TasksResource::make($tasks);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tasks $tasks)
+    public function destroy($id)
     {
-        //
+        $tasks = Tasks::find($id);
+        $status = $tasks->delete();
+        $tasks->refresh();
+        return response()->noContent();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function complete(Request $request, $id)
+    {
+        $tasks = Tasks::find($id);
+        $tasks->is_complited = $request->is_complited;
+        $tasks->save();
+
+        return TasksResource::make($tasks);
     }
 }
